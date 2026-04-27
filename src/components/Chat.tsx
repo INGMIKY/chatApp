c521cfd5ff739f05d88ea074815a84bd039d48a1
Miguel <migueltony9@gmail.com>
2026-04-26 18:50:13 -0600

    fix: vercel deploy

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 import { auth, db } from "../firebase";
-import { query, collection, orderBy, onSnapshot, Timestamp } from 'firebase/firestore'
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  Timestamp,
+} from "firebase/firestore";
 import Message from "./Message";
 import SendMessage from "./SendMessage";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -13,53 +19,47 @@ interface MessageType {
   photo: string;
 }
 
-
-
 const Chat = () => {
+  const [messages, setMessages] = useState<MessageType[]>([]);
+  const [user] = useAuthState(auth);
 
-    const [messages, setMessages] = useState<MessageType[]>([]);
-    const [user] = useAuthState(auth);
+  const getMessage = () => {
+    try {
+      const newQuery = query(collection(db, "messages"), orderBy("timestamp"));
 
-    
-    const getMessage =  () => {
-        try{
-            const newQuery = query(collection(db, 'messages'), orderBy('timestamp'));
-            
+      const unsubscribe = onSnapshot(newQuery, (querySpanshot) => {
+        const currentMessages: MessageType[] = [];
+        querySpanshot.forEach((item) => {
+          const data = item.data() as {
+            text: string;
+            timestamp: Timestamp;
+            uid: string;
+            photo: string;
+          };
 
-            const unsubscribe = onSnapshot(newQuery, (querySpanshot)=>{
-                const currentMessages: MessageType[]= [];
-                querySpanshot.forEach(item => {
+          currentMessages.push({ id: item.id, ...data });
+          console.log("datos:", item.data());
+        });
+        setMessages(currentMessages);
+      });
 
-                    const data = item.data() as { text: string; timestamp: Timestamp; uid: string; photo:string, };
-
-                    currentMessages.push({ id: item.id, ...data })
-                    console.log('datos:', item.data())
-                })
-                setMessages(currentMessages)
-            })
-
-            return unsubscribe;
-            
-        }catch(error){
-            console.error('Hubo un problema al conectarse a la db', error)
-        }
+      return unsubscribe;
+    } catch (error) {
+      console.error("Hubo un problema al conectarse a la db", error);
     }
-    
-    useEffect(()=> {
-        getMessage()
-    },[])
+  };
 
-    
+  useEffect(() => {
+    getMessage();
+  }, []);
 
-    return (
-        <section className="chat-content">
-            { messages && messages.map((item)=>(
-                <Message key={item.id} message={item}/>
-            ))}
-            {user && <SendMessage />}
-            
-        </section>
-    )
-}
+  return (
+    <section className="chat-content">
+      {messages &&
+        messages.map((item) => <Message key={item.id} message={item} />)}
+      {user && <SendMessage />}
+    </section>
+  );
+};
 
 export default Chat;
